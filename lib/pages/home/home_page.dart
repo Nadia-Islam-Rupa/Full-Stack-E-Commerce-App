0fe11ad86f89b4b05530517033d3311107d77b3e
Nadia Islam <sohasara853@gmail.com>
2026-01-21 17:44:49 +0600

    feat: update HomePage layout and add featured products section

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:ai_powered_e_commerce_app/data/banner/banner_data.dart';
 import 'package:ai_powered_e_commerce_app/data/category/category_data.dart';
+import 'package:ai_powered_e_commerce_app/data/featued_product/featured_data.dart';
 import 'package:ai_powered_e_commerce_app/pages/home/banner.dart';
 import 'package:ai_powered_e_commerce_app/pages/home/category.dart';
+import 'package:ai_powered_e_commerce_app/pages/home/featured_product.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,94 +16,107 @@ class HomePage extends StatelessWidget {
       backgroundColor: const Color(0xFFF5F5F5),
       body: Padding(
         padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Hello", style: TextStyle(fontSize: 17)),
-                    Text(
-                      "Welcome",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Hello", style: TextStyle(fontSize: 17)),
+                      Text(
+                        "Welcome",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.shopping_cart, size: 28),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.05),
-
-                    blurRadius: 10,
-                    offset: Offset(0, 4),
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.shopping_cart, size: 28),
                   ),
                 ],
               ),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Search for products",
-                  prefixIcon: Icon(Icons.search),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: 0.05),
+
+                      blurRadius: 10,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Search for products",
+                    prefixIcon: Icon(Icons.search),
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Categories",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                Text("view all ->", style: TextStyle(fontSize: 14)),
-              ],
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              height: 90,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: CategoryItem(category: categories[index]),
-                  );
-                },
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Categories",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text("view all ->", style: TextStyle(fontSize: 14)),
+                ],
               ),
-            ),
-            SizedBox(height: 20),
-            BannerSlider(banners: bannerImages),
-            SizedBox(height: 20),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Featured ",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              SizedBox(height: 20),
+              SizedBox(
+                height: 90,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: CategoryItem(category: categories[index]),
+                    );
+                  },
                 ),
-                Text("view all ->", style: TextStyle(fontSize: 14)),
-              ],
-            ),
-          ],
+              ),
+              SizedBox(height: 20),
+              BannerSlider(banners: bannerImages),
+              SizedBox(height: 20),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Featured ",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text("view all ->", style: TextStyle(fontSize: 14)),
+                ],
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                height: 230,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: featuredProducts.length,
+                  itemBuilder: (context, index) {
+                    return ProductCard(product: featuredProducts[index]);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
