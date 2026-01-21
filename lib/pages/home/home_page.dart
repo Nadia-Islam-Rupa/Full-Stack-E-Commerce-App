a4f442516a3d596cde4828c6a5f4922ac98875a7
Nadia Islam <sohasara853@gmail.com>
2026-01-21 15:37:57 +0600

    Refactor HomePage layout for improved styling and structure

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,26 +5,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(10.0),
         child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("hello"),
+                    Text("Hello", style: TextStyle(fontSize: 17)),
                     Text(
                       "Welcome",
                       style: TextStyle(
-                        fontSize: 24,
+                        fontSize: 22,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.shopping_cart, size: 28),
                 ),
               ],
             ),
