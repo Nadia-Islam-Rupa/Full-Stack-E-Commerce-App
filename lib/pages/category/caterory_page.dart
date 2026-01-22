3b478f65a9ae14859e3114bd6451401004db663e
Nadia Islam <sohasara853@gmail.com>
2026-01-22 21:33:59 +0600

    feat: add ShowCategory widget for category display

---
+++ lib/pages/category/caterory_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShowCategory extends StatelessWidget {
+  const ShowCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
