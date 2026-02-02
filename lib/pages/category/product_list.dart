81eb893ba6ce97e5639bd84e6677f379ae9d51d1
Nadia Islam <sohasara853@gmail.com>
2026-02-02 19:43:31 +0600

    feat: enhance ProductListPage layout with improved app bar and category display

--- lib/pages/category/product_list.dart
+++ lib/pages/category/product_list.dart
@@ -1,3 +1,5 @@
+import 'package:ai_powered_e_commerce_app/data/category/category_data.dart';
+import 'package:ai_powered_e_commerce_app/pages/home/category.dart';
 import 'package:flutter/material.dart';
 
 class ProductListPage extends StatelessWidget {
@@ -6,9 +8,29 @@ class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('category.name')),
-      body: Center(
-        child: Text("Products of", style: const TextStyle(fontSize: 18)),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Fashion',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 90,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: CategoryItem(category: categories[index]),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
