21bd0686d163da819ea6ec930518de4db3090e33
Nadia Islam <sohasara853@gmail.com>
2026-02-02 20:28:46 +0600

    feat: add subcategory data structure for product categorization

---
+++ lib/data/sub_category/sub_cat.dart
@@ -0,0 +1,7 @@
+final Map<String, List<String>> subcategory = {
+  'Fashion': ['All', 'Shirts', 'Pants', 'Shoes', 'Bags'],
+  'Beauty': ['All', 'Skincare', 'Makeup', 'Hair'],
+  'Mens': ['All', 'T-Shirts', 'Jeans', 'Shoes'],
+  'Womens': ['All', 'Dress', 'Bags', 'Jewelry'],
+  'Kids': ['All', 'Toys', 'Clothes', 'School'],
+};
