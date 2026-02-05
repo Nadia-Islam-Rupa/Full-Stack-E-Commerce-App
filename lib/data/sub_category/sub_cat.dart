399f58a85a8c5251682f6223e51bed207adf5089
Nadia Islam <sohasara853@gmail.com>
2026-02-05 13:25:31 +0600

    feat: add 'Footwear' to Womens subcategory in sub_cat.dart

--- lib/data/sub_category/sub_cat.dart
+++ lib/data/sub_category/sub_cat.dart
@@ -2,6 +2,6 @@ final Map<String, List<String>> subcategory = {
   'Fashion': ['All', 'Shirts', 'Pants', 'Shoes', 'Bags'],
   'Beauty': ['All', 'Skincare', 'Makeup', 'Hair'],
   'Mens': ['All', 'T-Shirts', 'Jeans', 'Shoes'],
-  'Womens': ['All', 'Dress', 'Bags', 'Jewelry'],
+  'Womens': ['All', 'Dress', 'Bags', 'Jewelry', 'Footwear'],
   'Kids': ['All', 'Toys', 'Clothes', 'School'],
 };
