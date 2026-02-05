30e015dbd1f6b07e9cff85fc4e3859e968e0586b
Nadia Islam <sohasara853@gmail.com>
2026-02-05 11:44:29 +0600

    feat: create FeatureProductCard component and update ProductListPage to use it

--- lib/pages/featured_products/product_list.dart
+++ lib/pages/featured_products/product_list.dart
@@ -1,5 +1,7 @@
 import 'package:ai_powered_e_commerce_app/data/sub_category/sub_cat.dart';
 import 'package:ai_powered_e_commerce_app/pages/category/provider/sub_cat_provider.dart';
+import 'package:ai_powered_e_commerce_app/pages/featured_products/product_card.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -60,50 +62,7 @@ class ProductListPage extends ConsumerWidget {
               ),
               itemCount: 5,
               itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 0.5, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          topRight: Radius.circular(12),
-                        ),
-                        child: Image.asset(
-                          "assets/men.jpeg",
-                          height: 160, // ⬅️ reduced height
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const SizedBox(height: 6),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8),
-                        child: Text(
-                          'Product Name',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                          ),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      const SizedBox(height: 2),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8),
-                        child: Text(
-                          'Price: \$99.99',
-                          style: TextStyle(fontSize: 13, color: Colors.grey),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
+                return FeatureProductCard();
               },
             ),
           ),
